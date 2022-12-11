bbe3722c3410c55ab76758efd613e5cb4e9c011f
mxrc78 <madlenermarc@gmail.com>
2022-12-11 20:56:11 +0100

    simple calculator
    A simple calculator, with 5 buttons. You can decide between "+", "-", "*", "/". First you can type in any number, press on a button, and then press on the equal button. Then the result appears in the input type

---
+++ vue/simple_calculator/src/components/SimpleButton.vue
@@ -0,0 +1,82 @@
+<template>
+   
+        <div class = "buttons">
+            <button @click="plus" class ="single-button" value="+" id="plus" type="text">+</button>
+            <button @click = "minus" class ="single-button"  id = "minus" type="text">-</button>
+            <button @click="mal" class ="single-button"  id = "mal" type="text">*</button>
+            <button @click="teilen" class ="single-button"  id = "teilen" type="text">/</button>
+        </div>
+        <div class="special">
+            <button @click="gleich" class ="single-button-is" type="text">=</button>        
+        </div>
+  </template>
+
+  <script>
+    export default{
+        emits: ['add-Calc'],
+       
+        methods:{
+           plus(){
+                this.$emit("add-Calc");
+            },
+            minus(){
+                this.$emit("minus-Calc");
+            },
+            mal(){
+                this.$emit("multiply-Calc");
+            },
+            teilen(){
+                this.$emit("divide-Calc");
+            },
+            gleich(){
+                this.$emit("equals-Calc");
+            }
+        },
+    }
+</script>
+<style>
+
+.buttons{
+    display:flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+}
+.single-button{
+    padding:20px 20px 20px 20px;
+    margin-top:40px;
+    background-color:orange;
+    border: 1px solid black;   
+    height: 50px;
+    width:60px;
+    box-shadow:10px 10px 10px 10px rgba(0, 0, 0, 0.2);
+}
+.special{
+    display:flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align:center;
+}
+
+.single-button-is{
+    background-color:orange;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 40px;
+    margin-right: 30px;
+    margin-top: 40px;
+    height: 50px;
+    width: 600px;
+    padding-left: 300px;
+    box-shadow:10px 10px 10px 10px rgba(0, 0, 0, 0.2);
+}
+.single-button:hover{
+    background-color:orangered;
+}
+.single-button-is:hover{
+    background-color:orangered;
+}
+body{
+    background-color:orangered;
+}
+</style>
---
+++ vue/simple_calculator/src/components/TheCalculator.vue
@@ -0,0 +1,105 @@
+  <template>
+   <div class="form-area">
+    <h1>Calculator</h1>
+    <div class = "input">     
+          <input class = "input-types" type="text" v-model="number" placeholder="Enter Calculation">
+          <simple-button
+          @add-calc="add"
+          @minus-calc="minus"
+          @multiply-calc="multiply"
+          @divide-calc="divide"
+          @equals-calc="equals"
+          ></simple-button>
+    </div>
+  </div>
+  </template>
+  <script>
+   
+import SimpleButton from './SimpleButton.vue'
+  export default {
+    name: 'App',
+    components: {
+        SimpleButton
+    },
+
+    data(){
+      return{
+      number1: "",
+      number2: "",
+      number: "",
+      operator: "",
+      result: "",
+      }
+     },        
+        methods: {
+          add(){
+            this.number1 = this.number;
+            this.number = "";
+            this.operator = "+";
+          },
+          minus(){
+            this.number1 = this.number;
+            this.number = "";
+            this.operator = "-";
+          },
+          multiply(){
+            this.number1 = this.number;
+            this.number = "";
+            this.operator = "*";
+          },
+          divide(){
+            this.number1 = this.number;
+            this.number = "";
+            this.operator = "/";
+          },
+          equals(){
+            this.number2 = this.number;
+        
+            switch(this.operator){
+            case "+":
+              this.result = parseInt(this.number1) + parseInt(this.number2); 
+              console.log(this.result);
+              break;
+            case "-":
+              this.result = parseInt(this.number1) - parseInt(this.number2);
+              console.log(this.result);
+              break;
+            case "*":
+              this.result = parseInt(this.number1) * parseInt(this.number2);
+              console.log(this.result);
+              break;
+            case "/":
+              this.result = parseInt(this.number1) / parseInt(this.number2);
+              console.log(this.result);
+              break;
+          }
+            this.number = this.result;
+        }
+       },
+      }
+  </script>
+  <style>
+
+.input-types{
+    height: 50px;
+    width: 600px;
+    margin-left:auto;
+    margin-right:auto;
+}
+h1{
+    padding-top:20px;
+}
+div.form-area{
+    background-color: grey;
+    height: 400px;
+    width: 700px;
+    margin-left: 300px;
+    margin-top: 150px;
+    box-shadow:5px;
+    border-radius:10px;
+    box-shadow:10px 10px 10px 10px rgba(0, 0, 0, 0.2);
+}
+body{
+    background-color:orangered;
+}
+  </style>
